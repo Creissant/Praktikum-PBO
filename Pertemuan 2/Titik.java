53e193ba6a43db83d04de5144bb674c45c9782c1
Varrel <100354535+Creissant@users.noreply.github.com>
2023-03-01 20:20:33 +0700

    Pertemuan 2
    Varrel_24060121130062

---
+++ Pertemuan 2/Titik.java
@@ -0,0 +1,45 @@
+/**
+ * File      : Titik.java		01/03/23
+ * Penulis   : Varrel
+ * Deskripsi : Kelas yang berisi body dari Titik
+**/
+
+class Titik {
+	double absis;
+	double ordinat;
+	static int counterTitik;
+	
+	//Tugas dalam post test
+	Titik(double a, double o){
+		counterTitik++;
+		absis = a;
+		ordinat = o;
+	}
+	
+	Titik() {
+		absis = 0;
+		ordinat = 0;
+		counterTitik++;
+	}
+	
+	void setAbsis(double a) {
+		absis = a;
+	}
+	
+	void setOrdinat(double o) {
+		ordinat = o;
+	}
+	
+	double getAbsis() {
+		return absis;
+	}
+	
+	double getOrdinat() {
+		return ordinat;
+	}
+	
+	int
+	getcounterTitik(){
+		return counterTitik;
+	}
+}
