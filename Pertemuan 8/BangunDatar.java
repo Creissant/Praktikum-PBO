2fa5a8bb485fbc771fe86e2d7da14c19cc0a066f
Varrel <100354535+Creissant@users.noreply.github.com>
2023-05-17 19:52:47 +0700

    Praktikum 8 (17-5-2023)
    Tugas:
    1. membuat kelas segitiga dan dijalankan pada BangunDatarGenerictest.java
    2. Cobalah pada saat praktikum : Gantilah ‘T’ (dan segala T) pada BangunDatarGeneric.java diganti dengan karakter yang lain seperti, T1,T2,T1234. Simpulkan apa yang anda lakukan !

---
+++ Pertemuan 8/BangunDatar.java
@@ -0,0 +1,10 @@
+/**
+ * File      : BangunDatar.java		03/05/23
+ * Penulis   : Varrel / 24060121130062
+ * Deskripsi : kelas abstrak untuk bangun datar
+ *
+ */
+
+public abstract class BangunDatar {
+    public abstract double hitungKeliling();
+}
